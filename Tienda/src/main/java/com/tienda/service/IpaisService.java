07e3151bb0529083a5a28351d7d35dd9f88f6cf1
gaysx <gaysx@LAPTOP-D6905IF0>
2023-02-14 21:03:28 -0600

    Quinta semana

---
+++ Tienda/src/main/java/com/tienda/service/IpaisService.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.tienda.service;
+
+import com.tienda.entity.pais;
+import java.util.List;
+
+/**
+ *
+ * @author gaysx
+ */
+public interface IpaisService {
+    public List <pais> listCountry();
+    
+    
+}
