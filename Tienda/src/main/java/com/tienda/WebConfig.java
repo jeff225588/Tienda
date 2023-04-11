85ca5070f62bcfd62d5c657b2f8b931116e7ccc4
gaysx <gaysx@LAPTOP-D6905IF0>
2023-04-11 15:16:02 -0600

    Setima semana

--- Tienda/src/main/java/com/tienda/WebConfig.java
+++ Tienda/src/main/java/com/tienda/WebConfig.java
@@ -4,7 +4,9 @@ package com.tienda;
 import java.util.Locale;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -13,32 +15,32 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 public class WebConfig implements WebMvcConfigurer {
     
     @Bean
-    public SessionLocaleResolver localeResolver(){
-      var slr= new SessionLocaleResolver();
-      slr.setDefaultLocale(new Locale("es"));
-      return slr;
-    
+    public LocaleResolver localeResolver() {
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(new Locale("es"));
+        return slr;
    }
+    
     @Bean
     public LocaleChangeInterceptor localeChangeInterceptor() {
-     var lci = new LocaleChangeInterceptor();
-     lci.setParamName("lang");
-     return lci;
-     
-     
-        
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
     }
     
     @Override
-    public void addIntercerceptors (InterceptorRegistry registro){
-        registro.addInterceptor(localeChangeInterceptor());
-        
-        
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
+    
     @Override
-    public void addViewController(ViewControllerRegistry)
-    
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/admin").setViewName("admin");
+        registry.addViewController("/403").setViewName("403");
     }
-    
+}
     
 
